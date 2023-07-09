3fa59a137197f3a8878ed1f46f94a7f1bec232b8
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-07-09 17:03:42 +0100

    init

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/Oluwaseun241/hexlock/internal"
+	"github.com/Oluwaseun241/hexlock/cmd"
 )
 
 func main() {
@@ -25,11 +25,11 @@ func main() {
   var err error 
   switch *mode {
   case "encrypt":
-    err = internal.EncryptFile(*inputFilePath, *outputFilePath, key)
+    err = cmd.EncryptFile(*inputFilePath, *outputFilePath, key)
   case "decrypt":
-    err = internal.DecryptFile(*inputFilePath, *outputFilePath, key)
+    err = cmd.DecryptFile(*inputFilePath, *outputFilePath, key)
   case "compress":
-    err = internal.CompressFile(*inputFilePath, *outputFilePath+".gz")
+    err = cmd.CompressFile(*inputFilePath, *outputFilePath+".gz")
   default:
     fmt.Println("Invalid mode")
     flag.PrintDefaults()
